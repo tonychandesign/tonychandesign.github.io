5f8c594f80bc9076d790c3561486419672f266d6
tonychan96 <tony_chan@intuit.com>
2020-01-27 23:40:07 -0800

    perminant slide in animation in index

--- custom.js
+++ custom.js
@@ -1,10 +1,5 @@
 // var menu_pageid = 0;
 $(function() {
-  // this is the shorthand for document.ready
-
-  // fade out animation
-  $("body").removeClass("fade-out");
-
   // Check for slide on new load
   // specifically it loads designated carousel item when navigating from proj page
   var query = window.location.search;
@@ -31,18 +26,6 @@ $(function() {
       defaultActive.toggleClass("active");
     }
   }
-
-  // SEMI STICKY NAVBAR
-  // $(document).scroll(function() {
-  //   // this is the scroll event for the document
-  //   scrolltop = $(document).scrollTop(); // by this we get the value of the scrolltop ie how much scroll has been don by user
-  //   if (parseInt(scrolltop) >= 60) {
-  //     // check if the scroll value is equal to the top of navigation
-  //     $("#navbar").css({ position: "fixed", top: "0" }); // is yes then make the position fixed to top 0
-  //   } else {
-  //     $("#navbar").css({ position: "absolute", top: "60px" }); // if no then make the position to absolute and set it to 80
-  //   }
-  // });
 
   var prevScrollpos = window.pageYOffset;
   window.onscroll = function() {
@@ -86,36 +69,5 @@ $(function() {
       active.toggleClass("active ");
       link.parent().toggleClass("active ");
     }
-  });
-
-  // Module move up animation
-  (function($) {
-    $.fn.visible = function(partial) {
-      var $t = $(this),
-        $w = $(window),
-        viewTop = $w.scrollTop(),
-        viewBottom = viewTop + $w.height(),
-        _top = $t.offset().top,
-        _bottom = _top + $t.height(),
-        compareTop = partial === true ? _bottom : _top,
-        compareBottom = partial === true ? _top : _bottom;
-      return compareBottom <= viewBottom && compareTop >= viewTop;
-    };
-  })(jQuery);
-  var win = $(window);
-  var allMods = $(".module");
-  allMods.each(function(i, el) {
-    var el = $(el);
-    if (el.visible(true)) {
-      el.addClass("already-visible");
-    }
-  });
-  win.scroll(function(event) {
-    allMods.each(function(i, el) {
-      var el = $(el);
-      if (el.visible(true)) {
-        el.addClass("come-in");
-      }
-    });
   });
 });
