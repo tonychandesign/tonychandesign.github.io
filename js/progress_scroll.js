aa131c31b0903cb73534b8d9b0dc355807f43596
tonychan96 <tony_chan@intuit.com>
2020-02-05 20:35:46 -0800

    made scroll progress bar flex and mobile responsive

--- js/progress_scroll.js
+++ js/progress_scroll.js
@@ -5,5 +5,14 @@ $(function() {
     $("div#scroll-bar").width(
       ($w.scrollTop() / ($d.height() - $w.height())) * 1650
     );
+    // Update the width of the progress bar
+    var thanksTop =
+      $(".END").offset().top -
+      window.innerHeight * 0.8 -
+      $(".START").offset().top;
+    var progressBarTop =
+      $("#scroll-bar").offset().top - $(".START").offset().top;
+    var percentage = (progressBarTop / thanksTop) * 100;
+    $("#scroll-bar").css("width", percentage + "vw");
   });
 });
