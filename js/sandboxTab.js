c421944680beaa6f2893266f4e81bd763e75b51b
tonychan96 <tony_chan@intuit.com>
2020-06-14 21:08:29 -0700

    added sandbox and subnav, replaced hero animation with just text

---
+++ js/sandboxTab.js
@@ -0,0 +1,42 @@
+// handle carousel
+
+$(function () {
+  var photoTab = $("#sandboxPhoto");
+  var photoEle = $(".sandbox_photography");
+  var codeTab = $("#sandboxCoding");
+  var designTab = $("#sandboxDesign");
+  var motionTab = $("#sandboxMotion");
+
+  var comingSoon = $("#sandbox_comingSoon");
+
+  photoTab.click(function () {
+    photoTab.addClass("active");
+    codeTab.removeClass("active");
+    designTab.removeClass("active");
+    motionTab.removeClass("active");
+
+    photoEle.css({ display: "block" });
+    comingSoon.css({ display: "none" });
+  });
+  codeTab.click(function () {
+    codeTab.addClass("active");
+    photoTab.removeClass("active");
+    designTab.removeClass("active");
+    motionTab.removeClass("active");
+
+    photoEle.css({ display: "none" });
+    comingSoon.css({ display: "block" });
+  });
+  designTab.click(function () {
+    designTab.addClass("active");
+    codeTab.removeClass("active");
+    photoTab.removeClass("active");
+    motionTab.removeClass("active");
+  });
+  motionTab.click(function () {
+    motionTab.addClass("active");
+    codeTab.removeClass("active");
+    photoTab.removeClass("active");
+    designTab.removeClass("active");
+  });
+});
