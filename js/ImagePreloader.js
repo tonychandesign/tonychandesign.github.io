277965f5c7ed9ef40fb94a55fde649c8989705ce
tonychan96 <tony_chan@intuit.com>
2020-07-15 17:24:00 -0700

    preload pngs too

--- js/ImagePreloader.js
+++ js/ImagePreloader.js
@@ -22,6 +22,21 @@ $(document).ready(function () {
     "img/wire_covers/wire_nanome.svg",
     "img/wire_covers/wire_nanome_mobile.svg",
     "img/wire_covers/wire_workday.svg",
-    "img/wire_covers/wire_workday_mobile.svg"
+    "img/wire_covers/wire_workday_mobile.svg",
+
+    "img/wire_covers/asgs.png",
+    "img/wire_covers/asgs_mobile.png",
+    "img/wire_covers/birch.png",
+    "img/wire_covers/birch_mobile.png",
+    "img/wire_covers/d4sd.png",
+    "img/wire_covers/d4sd_mobile.png",
+    "img/wire_covers/eqr.png",
+    "img/wire_covers/eqr_mobile.png",
+    "img/wire_covers/intuit.png",
+    "img/wire_covers/intuit_mobile.png",
+    "img/wire_covers/nanome.png",
+    "img/wire_covers/nanome_mobile.png",
+    "img/wire_covers/workday.png",
+    "img/wire_covers/workday_mobile.png"
   );
 });
